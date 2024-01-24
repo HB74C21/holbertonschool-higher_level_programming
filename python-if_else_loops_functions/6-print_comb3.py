c43b74f6d69fb1387b736c06d7d6ccfe479bb769
HB74C21 <6720@holbertonstudents.com>
2024-01-24 22:22:07 +0000

    Add new program that prints all possible different combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for number_left in range(0, 8):
+    for number_right in range(1, 10):
+        if number_left < number_right:
+            print("{}""{}"", ".format(number_left, number_right), end="")
+print("89")

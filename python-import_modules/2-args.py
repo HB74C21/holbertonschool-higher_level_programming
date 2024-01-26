77944bff65193099f2573fe01f763c142811363d
HB74C21 <6720@holbertonstudents.com>
2024-01-26 15:55:31 +0000

    fix add if __name__ == "__main__":

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-count_arg = sys.argv[1:]
-length_argv = 1
+    count_arg = sys.argv[1:]
+    length_argv = 1
 
-if len(count_arg) < 1:
-    print("{} arguments.".format(len(count_arg)))
-elif len(count_arg) == 1:
-    print("{} argument:".format(len(count_arg)))
-    print("{}: {}".format(len(count_arg), sys.argv[length_argv]))
-else:
-    print("{} arguments:".format(len(count_arg)))
-    while length_argv <= len(count_arg):
-        print("{}: {}".format(length_argv, sys.argv[length_argv]))
-        length_argv += 1
+    if len(count_arg) < 1:
+        print("{} arguments.".format(len(count_arg)))
+    elif len(count_arg) == 1:
+        print("{} argument:".format(len(count_arg)))
+        print("{}: {}".format(len(count_arg), sys.argv[length_argv]))
+    else:
+        print("{} arguments:".format(len(count_arg)))
+        while length_argv <= len(count_arg):
+            print("{}: {}".format(length_argv, sys.argv[length_argv]))
+            length_argv += 1

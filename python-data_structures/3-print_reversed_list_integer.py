35fef4dd3f6b6357536e576c6fcbdbe5721a8105
HB74C21 <6720@holbertonstudents.com>
2024-01-29 14:48:47 +0000

    change the syntaxe

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    index = len(my_list)
-    while index > 0:
-        print("{:d}".format(my_list[index - 1]))
-        index -= 1
+    index = my_list.reverse()
+    for index in my_list:
+        print("{}".format(index))

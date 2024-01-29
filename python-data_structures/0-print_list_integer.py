ff6890b2289b286c63497007bead7e18b7d8878a
HB74C21 <6720@holbertonstudents.com>
2024-01-29 12:00:48 +0000

    Change new algo for task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    count = 0
-    while count < len(my_list):
-        print("{:d}".format(my_list[count]))
-        count += 1
+    for count in my_list:
+        print("{:d}".format(count))

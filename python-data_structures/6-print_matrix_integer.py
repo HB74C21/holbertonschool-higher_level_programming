fa106c435280df02ea9f6ac259c5b0977ea0afdf
HB74C21 <6720@holbertonstudents.com>
2024-01-31 09:31:43 +0000

    Add 2 condition for not print a space at the end of the line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for colum in row:
-            print("{:d}".format(colum), end=" ")
+            if colum == row[-1]:
+                print("{:d}".format(colum), end="")
+            else:
+                print("{:d}".format(colum), end=" ")
         print()

bd32b010eaa1ce0ffaa982ca58070a1bc859eb9d
HB74C21 <6720@holbertonstudents.com>
2024-02-01 13:33:13 +0000

    fix error of out

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         new_row = []
         for j in range(len(matrix[0])):
-            new_row.append(matrix[i][j] * 2)
+            new_row.append(matrix[i][j] * matrix[i][j])
         new_matrix.append(new_row)
     return new_matrix

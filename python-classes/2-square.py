3f1c908e4044698954f98f4c08e660aa9acd1197
HB74C21 <6720@holbertonstudents.com>
2024-02-05 15:23:14 +0000

    Function with a class Square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+class Square:
+    """A class for squares"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")

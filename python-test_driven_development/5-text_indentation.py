beca9e8fb3bc8d82355e7f249e7856f153018046
HB74C21 <6720@holbertonstudents.com>
2024-02-08 23:28:14 +0000

    Add task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    This module defines a function to print a text with
+    2 new lines after each occurrence of '.', '?' and ':'
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each occurrence of '.', '?' and ':'
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation = ['.', '?', ':']
+    for char in text:
+        print(char, end="")
+        if char in punctuation:
+            print("\n")
+    print()

7bad08f9686a8abd0d0199051ef6be96adf9127f
HB74C21 <6720@holbertonstudents.com>
2024-02-08 11:10:09 +0000

    Add function that add 2 integer + Add add files with test error

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    This script defines a function named add_integer that adds two numbers.
+    It ensures that both input parameters are integers or floats and converts
+    them to integers if necessary before performing the addition.
+"""
+
+
+def add_integer(a, b=98):
+    """
+        This function takes two parameters, a and b, and adds them together.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b

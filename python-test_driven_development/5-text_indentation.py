2bf8c0ddfeaec9bc33b323a2176c55cba3a1e705
HB74C21 <6720@holbertonstudents.com>
2024-02-11 19:16:41 +0000

    fix syntax

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-This module defines a function to print
-a text with 2 new lines after each
-occurrence of '.', '?' and ':'
+    This module defines a function to print
+    a text with 2 new lines after each
+    occurrence of '.', '?' and ':'
 """
 
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each occurrence of '.', '?' and ':'
+        Prints a text with 2 new lines after each occurrence of '.', '?' and ':'
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    punctuation = ['.', '?', ':']
+    punctuation = [".", "?", ":"]
+    new_line = True
     for char in text:
-        print(char, end="")
-        if char in punctuation:
-            print("\n")
+        if new_line and char == ' ':
+            continue
+        elif char == "\n":
+            new_line = True
+        elif char in punctuation:
+            print(char + "\n\n", end="")
+            new_line = True
+        else:
+            print(f"{char}", end="")
+            new_line = False
     print()

0172ba1e36faefc864aa89e598be140f7bb8c34e
HB74C21 <6720@holbertonstudents.com>
2024-02-11 17:39:24 +0000

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-    This script defines a function named add_integer that adds two numbers.
-    It ensures that both input parameters are integers or floats and converts
-    them to integers if necessary before performing the addition.
+This script defines a function named add_integer that adds two numbers.
+It ensures that both input parameters are integers or floats and converts
+them to integers if necessary before performing the addition.
 """
 
 
 def add_integer(a, b=98):
     """
-        This function takes two parameters, a and b, and adds them together.
+    This function takes two parameters, a and b, and adds them together.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")

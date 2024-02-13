6b9ccf72b3d7459d5abe0a8fcac44d7e18b2ad85
HB74C21 <6720@holbertonstudents.com>
2024-02-13 15:08:14 +0000

    Add for task 5:  Write an empty class BaseGeometry.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script defines a base class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class
+    """
+
+    def __init__(self):
+        pass

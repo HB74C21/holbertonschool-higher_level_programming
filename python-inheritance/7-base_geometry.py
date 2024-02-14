22e1accfe840ec9430546f879b51a5feba97ef5a
HB74C21 <6720@holbertonstudents.com>
2024-02-14 10:00:25 +0000

    Fix add documention

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-
+This script defines a base class called BaseGeometry.
 """
 
 
 class BaseGeometry:
     """
-
+    This is the BaseGeometry class.
     """
 
     def __init__(self):

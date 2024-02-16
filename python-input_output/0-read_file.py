aaad5d272b27c114ec2cd619627d8139b40f8406
HB74C21 <6720@holbertonstudents.com>
2024-02-16 14:31:06 +0000

    Add end=""

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,4 @@ def read_file(filename=""):
     A function to read the contents of a file.
     """
     with open(filename, encoding="utf-8") as fd:
-        read_data = fd.read()
-
-        print(read_data)
+        print(fd.read(), end="")

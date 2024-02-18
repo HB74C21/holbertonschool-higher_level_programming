508ec918a758ada686b171b7a16ccbef59f85f63
HB74C21 <6720@holbertonstudents.com>
2024-02-18 16:23:28 +0000

    Add for task 6: Write a function that creates an Object from a “JSON file”:

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that creates an Object from a “JSON file”:
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load a JSON file into a Python object.
+    """
+    with open(filename) as fd:
+        data = json.load(fd)
+
+        return data

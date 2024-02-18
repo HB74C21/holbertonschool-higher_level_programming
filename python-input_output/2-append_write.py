65311d524d3b999bab0ae8305dc4b55d5368cde7
HB74C21 <6720@holbertonstudents.com>
2024-02-18 13:00:20 +0000

    Add for task 2: Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added:
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends text to the end of a text file (UTF8)
+    """
+    with open(filename, "a", encoding="utf-8") as fd:
+        new_file = fd.write(text)
+    return new_file

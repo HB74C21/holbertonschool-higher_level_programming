c6f175e73d40ce8446e4a8ffcdb395f5537223ee
HB74C21 <6720@holbertonstudents.com>
2024-02-28 22:04:42 +0000

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,11 +25,13 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.__width = value
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -37,11 +39,13 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.__height = value
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -49,11 +53,13 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.__x = value
+
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -61,11 +67,12 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.__y = value
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
 
     def area(self):
         return self.__width * self.__height
@@ -75,5 +82,5 @@ class Rectangle(Base):
             print("#" * self.__width)
 
     def __str__(self):
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
-                                                        self.__width, self.__height))
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)

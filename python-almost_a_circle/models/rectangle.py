7c95f5ebf15253b22c8d018117de7e70a4e91e57
HB74C21 <6720@holbertonstudents.com>
2024-02-28 22:11:50 +0000

    test checker

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,13 +25,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -39,13 +33,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -53,13 +41,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -67,20 +49,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def display(self):
-        for i in range(self.__height):
-            print("#" * self.__width)
-
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                       self.width, self.height)
+        self.__y = value

caa25a40b6af22d3beedf6018bcd6a9524e12aa7
HB74C21 <6720@holbertonstudents.com>
2024-02-29 17:19:24 +0000

    fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,11 +14,9 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a Square object with a given size, position, and ID."""
-        self.size = size
-        self.x = x
-        self.y = y
 
-        super().__init__(self.size, self.size, self.x, self.y, id)
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Return a string representation of the Square object."""

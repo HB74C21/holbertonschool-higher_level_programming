a7889fe90ab049b267b33f63ec4927f0d86a313a
HB74C21 <6720@holbertonstudents.com>
2024-03-04 10:35:30 +0000

    Add for task 17: Update the class Base by adding the static method def from_json_string(json_string): that returns the list of the JSON string representation json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-
+        Write the JSON string representation of list_objs to a file
         """
         filename = cls.__name__ + ".json"
 
@@ -48,3 +48,13 @@ class Base:
         with open(filename, "w") as f:
             f.write(cls.to_json_string(
                 [obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Convert a JSON string into a list of objects
+        """
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
